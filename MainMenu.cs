efa80d7ef8c032153c0d34bb7fec784b7d10c7a3
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-07-23 19:14:08 -0700

    added basic main menu scene and script for navigation and entry point

--- MainMenu.cs
+++ MainMenu.cs
@@ -3,30 +3,19 @@ using System;
 
 public partial class MainMenu : Control
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        // Grab focus for the start button
-        GetNode<Button>("VBoxContainer/StartButton").GrabFocus();
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
-
-    public void _on_start_button_pressed()
-    {
+    public void _on_start_pressed () {
+        GD.Print("start pressed.");
         GetTree().ChangeSceneToFile("res://playerTestScene.tscn");
+
     }
 
-    public void _on_options_button_pressed()
-    {
-        GD.Print("Options button pressed");
+    public void _on_options_pressed () {
+        GD.Print("options");
+        GetTree().ChangeSceneToFile("res://Options.tscn");
     }
 
-    public void _on_quit_button_pressed()
-    {
+    public void _on_quit_pressed () {
+        GD.Print("quit pressed");
         GetTree().Quit();
     }
 }

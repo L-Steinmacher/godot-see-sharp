dad7e3455008c594616404ef5de78ae7b68d6883
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-07-30 12:02:27 -0700

    turret make pew pew on player enter detection radious, cooldown timer working.

--- Turret.cs
+++ Turret.cs
@@ -3,21 +3,61 @@ using System;
 
 public partial class Turret : RigidBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
-    private void _on_detection_radious_body_entered(Node2D body) {
-        GD.Print("body: " + body.Name + " has entered the detection radious");
+    private double attackCooldown = 1.5;
+    private double attackCooldownReset = 1.5;
+    private bool isAttacking = false;
+    private bool active;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
     }
 
-    private void _on_detection_radious_body_exited(Node2D body) {
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        ProcessTimers(delta);
+        ProcessAttack();
+    }
+
+    private void ProcessAttack()
+    {
+        if (active)
+        {
+            if (!isAttacking)
+            {
+                GD.Print("Pew pew");
+                isAttacking = true;
+            }
+        }
+    }
+
+    private void ProcessTimers(double delta)
+    {
+        if (isAttacking)
+        {
+            attackCooldown -= delta;
+            if (attackCooldown <= 0)
+            {
+                isAttacking = false;
+                attackCooldown = attackCooldownReset;
+            }
+        }
+    }
+    private void _on_detection_radious_body_entered(Node2D body)
+    {
+        GD.Print("body: " + body.Name + " has entered the detection radious");
+        if (body is PlayerController)
+        {
+            active = true;
+        }
+    }
+
+    private void _on_detection_radious_body_exited(Node2D body)
+    {
         GD.Print("body: " + body.Name + " has exited the detection radious");
+        if (body is PlayerController)
+        {
+            active = false;
+        }
     }
 }

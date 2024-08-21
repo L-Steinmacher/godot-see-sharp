9373b004664802fd07ff2bf00467b5b1fa314864
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-08-21 15:32:23 -0700

    feat: battie patrolling in figure 8

--- Enemies/Battie/Battie.cs
+++ Enemies/Battie/Battie.cs
@@ -13,10 +13,12 @@ public partial class Battie : Enemy
     }
 
     public EnemyState CurrentState { get; set; } = EnemyState.Patrolling;
-    public const float Speed = 300.0f;
-    public const float JumpVelocity = -400.0f;
+    public const float Speed = 2.0f;
 
     private AnimatedSprite2D _animatedSprite2D;
+    private float _timeElapsed = 0.0f;
+    private float _figure8Width = 50.0f;
+    private float _figure8Height = 25.0f;
 
     public override void _Ready()
     {
@@ -35,7 +37,7 @@ public partial class Battie : Enemy
             case EnemyState.Idle:
                 break;
             case EnemyState.Patrolling:
-                ProcessPatrolling(delta);
+                velocity = ProcessPatrolling(delta);
                 break;
             case EnemyState.TakingDamage:
                 break;
@@ -49,19 +51,26 @@ public partial class Battie : Enemy
 
         Velocity = velocity;
         MoveAndSlide();
+
+        Vector2 ProcessPatrolling(double delta)
+        {
+            Vector2 velocity;
+            _timeElapsed += (float)delta;
+            float x = _figure8Width * Mathf.Sin(_timeElapsed);
+            float y = _figure8Height * Mathf.Cos(_timeElapsed * 2);
+
+            velocity = new Vector2(x, y);
+            return velocity;
+        }
     }
 
-    public void ProcessPatrolling(double delta)
-    {
-        // Patrolling logic to be implemented
-    }
 
     public override void TakeDamage(int damageTakenAmount)
     {
         EnemyState previousState = CurrentState;
         CurrentState = EnemyState.TakingDamage;
 
-        Health -= damageTakenAmount; // Use the passed argument
+        Health -= damageTakenAmount;
 
         if (Health <= 0)
         {
